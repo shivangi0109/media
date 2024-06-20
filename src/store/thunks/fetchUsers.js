d725979732ec65fa8496a26172399bfa516add2f
Shivangi Srivastava <shi06dec@gmail.com>
2024-06-20 15:05:18 -0600

    Wrappinf up the thunk

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const fetchUsers = createAsyncThunk('users/fetch', async () => {
-  const response = await axios.get('http://localhost/users');
+  const response = await axios.get('http://localhost:3005/users');
 
   return response.data;
 });
