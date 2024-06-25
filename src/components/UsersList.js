31fc4fb524f039687b802ce521554930ca72a756
Shivangi Srivastava <shi06dec@gmail.com>
2024-06-25 14:57:56 -0600

    Rendering the List of Users

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -22,9 +22,19 @@ function UsersList() {
     return <div>Error fetching data...</div>
   }
 
+  const renderedUsers = data.map((user) => {
+    return (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div>
-      {data.length}
+      {renderedUsers}
     </div>
   );
 }
