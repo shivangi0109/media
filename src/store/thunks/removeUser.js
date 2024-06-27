f640a82eca2c9218191c5790a17b10ff1c90a62d
Shivangi Srivastava <shi06dec@gmail.com>
2024-06-27 15:26:33 -0600

    Deleting the User

--- src/store/thunks/removeUser.js
+++ src/store/thunks/removeUser.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const removeUser = createAsyncThunk('users/remove', async (user) => {
-  const response = await axios.delete(`http://localhost:3005/user/${user.id}`);
+  const response = await axios.delete(`http://localhost:3005/users/${user.id}`);
 
   // FIX !!!!
   return response.data;
