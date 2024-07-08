693762d902e34eb45da8348a9dc4b433fb864df1
Shivangi Srivastava <shi06dec@gmail.com>
2024-07-08 16:46:57 -0600

    Create an Endpoint

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -4,5 +4,20 @@ const albumsApi = createApi({
   reducerPath: 'albums',
   baseQuery: fetchBaseQuery({
     baseUrl: 'http://localhost:3005'
-  })
+  }),
+   endpoints(builder) {
+    return {
+      fetchAlbums: builder.query({
+        query: (user) => {
+          return {
+            url: '/albums',
+            params: {
+              userId: user.id
+            },
+            method: 'GET'
+          };
+        },
+      }),
+    };
+  },
 });
