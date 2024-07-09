98e10135f15aaa3e00edeec6856aad6b1155bca9
Shivangi Srivastava <shi06dec@gmail.com>
2024-07-09 14:19:11 -0600

    Render the List

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,12 +1,33 @@
 import { useFetchAlbumsQuery } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
 
 const AlbumsList = ({ user }) => {
   const {data, error, isLoading } = useFetchAlbumsQuery(user);
 
-  console.log(data, error, isLoading);
+  let content;
+  if(isLoading) {
+    content = <Skeleton times={3} />
+  } else if (error) {
+    content = <div>Error loading albums.</div>
+  } else {
+    content = data.map(album => {
+      const header = <div>{album.title}</div>
+
+      return <ExpandablePanel key={album.id} header={header} >
+        List of photos in the album
+      </ExpandablePanel>
+    });
+  }
 
   return (
-    <div>Albums for {user.name}</div>
+    <div>
+      <div>
+        Albums for {user.name}
+      </div>
+      <div>{content}</div>
+    </div>
   );
 };
 
