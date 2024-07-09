22289d5e09fdc2ffb613406bd4caa41c67bdd9c4
Shivangi Srivastava <shi06dec@gmail.com>
2024-07-09 14:53:16 -0600

    Change data with Mutations

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,11 +1,16 @@
-import { useFetchAlbumsQuery } from "../store";
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
 import Skeleton from "./Skeleton";
 import ExpandablePanel from "./ExpandablePanel";
 import Button from "./Button";
 
 const AlbumsList = ({ user }) => {
   const {data, error, isLoading } = useFetchAlbumsQuery(user);
+  const [addAlbum, results] = useAddAlbumMutation();
 
+  const handleAddAlbum = () => {
+    addAlbum(user);
+  };
+  
   let content;
   if(isLoading) {
     content = <Skeleton times={3} />
@@ -25,6 +30,9 @@ const AlbumsList = ({ user }) => {
     <div>
       <div>
         Albums for {user.name}
+        <Button onClick={handleAddAlbum}>
+          + Add Album
+        </Button>
       </div>
       <div>{content}</div>
     </div>
