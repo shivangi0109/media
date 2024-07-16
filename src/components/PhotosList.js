48bb0a5c595bb453f63f0f7064dc7956575f5394
Shivangi Srivastava <shi06dec@gmail.com>
2024-07-16 14:10:30 -0600

    Create the  photo

--- src/components/PhotosList.js
+++ src/components/PhotosList.js
@@ -1,9 +1,22 @@
-import { useFetchPhotosQuery } from "../store";
+import { useFetchPhotosQuery, useAddPhotoMutation } from "../store";
+import Button from "./Button";
 
 const PhotosList = ({ album }) => {
   useFetchPhotosQuery(album);
+  const [addPhoto, addPhotoResults] = useAddPhotoMutation();
 
-  return <div>List of photos in the album</div>
+  const handleAddPhoto = () => {
+    addPhoto(album);
+  };
+
+  return (
+    <div>
+      <div className="m-2 flex flex-row items-center justify-between">
+        <h3 className="text-lg font-bold">Photos in {album.title}</h3>
+        <Button loading={addPhotoResults.isLoading} onClick={handleAddPhoto}>+ Add Photo</Button>
+      </div>
+    </div>
+  );
 };
 
 export default PhotosList;
